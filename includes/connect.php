aaf6aae909f7df401a5afbda72fa63089e291f00
Freddy Saez <freddysae00@gmail.com>
2022-07-16 10:56:50 -0400

    Here you will put your db details

---
+++ includes/connect.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$servername = "localhost";
+$server_user = "";
+$server_pass = "";
+$dbname = "food";
+$name = $_SESSION['name'];
+$role = $_SESSION['role'];
+$con = new mysqli($servername, $server_user, $server_pass, $dbname);
